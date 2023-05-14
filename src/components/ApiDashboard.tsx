2e9a567a11f5c4c56309029b3ebef7efaafa17fc
devgenx <sebastian110420@gmail.com>
2023-05-14 02:03:34 -0700

    Added revoking key functionality

--- src/components/ApiDashboard.tsx
+++ src/components/ApiDashboard.tsx
@@ -8,6 +8,7 @@ import LargeHeading from "./ui/LargeHeading";
 import Paragraph from "@/ui/Paragraph";
 import { Input } from "@/ui/Input";
 import Table from "@/components/Table";
+import ApiKeyOptions from "@/components/ApiKeyOptions";
 
 const ApiDashboard = async () => {
   const user = await getServerSession(authOptions);
@@ -43,13 +44,14 @@ const ApiDashboard = async () => {
       <div className="flex flex-col md:flex-row gap-4 justify-center md:justify-start items-center">
         <Paragraph>Your API Key</Paragraph>
         <Input className="w-fit truncate" readOnly value={activeApiKey.key} />
+        <ApiKeyOptions apiKeyId={activeApiKey.id} apiKeys={activeApiKey.key} />
       </div>
 
       <Paragraph className="text-center md:text-left mt-4 -mb-4">
         Your API History
       </Paragraph>
 
-      <Table />
+      <Table userRequests={serializableReq} />
     </div>
   );
 };
---
+++ src/components/ApiKeyOptions.tsx
@@ -0,0 +1,111 @@
+"use client";
+import { FC, useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/ui/DropdownMenu";
+import Button from "@/ui/Button";
+import { Loader2 } from "lucide-react";
+import { DropdownMenuContent } from "@radix-ui/react-dropdown-menu";
+import { toast } from "@/ui/Toast";
+import { createApiKey } from "@/helpers/create-api-key";
+import { revokeApiKey } from "@/helpers/revoke-api-key";
+import { useRouter } from "next/navigation";
+
+interface ApiKeyOptionsProps {
+  apiKeyId: string;
+  apiKeys: string;
+}
+
+const ApiKeyOptions: FC<ApiKeyOptionsProps> = ({ apiKeyId, apiKeys }) => {
+  const router = useRouter();
+
+  const [isCreatingNew, setIsCreatingNew] = useState<boolean>(false);
+  const [isRevoking, setIsRevoking] = useState<boolean>(false);
+
+  const createNewApiKey = async () => {
+    setIsCreatingNew(true);
+
+    try {
+      await revokeApiKey({ keyId: apiKeyId });
+      await createApiKey();
+      toast({
+        title: "Create API Key",
+        message: "Successfully created API key",
+        type: "success",
+      });
+      router.refresh();
+    } catch (err) {
+      toast({
+        title: "Failed to create API key",
+        message: "Please try again after 1 min",
+        type: "error",
+      });
+    } finally {
+      setIsCreatingNew(false);
+    }
+  };
+
+  const revokeKeys = async () => {
+    setIsRevoking(true);
+    try {
+      await revokeApiKey({ keyId: apiKeyId });
+      toast({
+        title: "Revoke API Key",
+        message: "Successfully revoked API key",
+        type: "success",
+      });
+      router.refresh();
+    } catch (err) {
+      toast({
+        title: "Failed to revoke API key",
+        message: "Please try again after 1 min",
+        type: "error",
+      });
+    } finally {
+      setIsRevoking(false);
+    }
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger disabled={isCreatingNew || isRevoking} asChild>
+        <Button variant={"ghost"} className="flex gap-2 items-center">
+          <p>
+            {isCreatingNew
+              ? "Creating new key..."
+              : isRevoking
+              ? "Revoking key..."
+              : "Options"}
+          </p>
+          {isCreatingNew || isRevoking ? (
+            <Loader2 className="animate-spin h-4 w-4" />
+          ) : null}
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent>
+        <DropdownMenuItem
+          onClick={() => {
+            navigator.clipboard.writeText(apiKeys);
+
+            toast({
+              title: "Copied",
+              message: "API Key copied to clipboard",
+              type: "success",
+            });
+          }}
+        >
+          Copy
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={createNewApiKey}>
+          Create API Key
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={revokeKeys}>Revoke API Key</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default ApiKeyOptions;
